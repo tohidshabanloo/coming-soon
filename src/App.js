527bbe390e8c28f3b31efd175992bd9f4ea7d362
tohidshabanloo <tshabanloo@hotmail.com>
2022-04-03 19:11:43 +0430

    added Footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Countdown from "./components/Countdown/Countdown";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <Countdown />
+      <Footer />
     </div>
   );
 }
